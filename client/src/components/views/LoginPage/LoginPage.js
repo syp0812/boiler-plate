571cec2d8d6ecc9e94cea4de74110f86e9d7970a
root <root@DESKTOP-FRD94OB.localdomain>
2022-04-12 15:27:32 +0900

    update

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../../_actions/user_actions';
 import { Form, Input, Button } from 'antd';
 
 function LoginPage() {
